9fef48cb5823bc59ff49d1975166a1a4a1cd33f4
mrMain371 <luisitogvalle@gmail.com>
2025-01-23 15:44:02 -0600

    se trabajo con la sintaxis de  operadores e java

--- Practica2/src/Main.java
+++ Practica2/src/Main.java
@@ -1,15 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how Aqua suggests fixing it.
-        System.out.printf("hola soy luis y estas en la filmo ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//zona de importacion
+// clase principal
+public class Main {
+    //metodo main
+    public static void main(String[] args) {
+
+String cadena= "luis"+ "cesar"+ "garduno "+ "valle";
+//imprimimos
+        System.out.println(cadena);
+        //contamos
+        System.out.println(cadena.length());
+        //cortamos segun el indice
+        System.out.println(cadena.substring(2,5));
+        System.out.println(cadena.substring(2));
+        System.out.println(cadena.substring(0,5));
+
+        //variables
+        String cadena1 = "333",cadena2,cadena3;
+        //convertir cadena a int
+        int entero1 = Integer.parseInt(cadena1);
+        System.out.println(entero1);
+        //convertir double a int
+        double decimal =7.7;
+       int entero3=(int)decimal;
+        System.out.println(entero3);
+cadena2= String.valueof(decimal);
+        System.out.println(cadena2);
+        //5. Boolean, operadores logico y de comparacion
+
+        //Los operadores logicos siempre devuelven un resultado booleano
+        System.out.println(10 > 9);
+        System.out.println(10 == 9);
+        System.out.println(10 < 9);
+        System.out.println(10 >= 9);
+        System.out.println(10 <= 9);
+        System.out.println(10 != 9);
+
+        int x= 3;
+        System.out.println(x< 5 && x>10);
+        System.out.println(x<5 || x>10);
+        System.out.println(!(x< 5 && x>10));
+        System.out.println(!(x<5 || x>10));
     }
 }
