a0fa4e6121e47ffdbf04aa9a5b9211b6631d56e2
mrMain371 <luisitogvalle@gmail.com>
2025-01-30 14:33:15 -0600

    se realizo el ejercicio 3

--- practica 4/src/ejercicios/ejercicios.java
+++ practica 4/src/ejercicios/ejercicios.java
@@ -27,4 +27,18 @@ public static void ejercicio1(){
           System.out.println("es impar");
       }
     }
+    public static void ejercicio3(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+
+        if (edad < 4) {
+            System.out.println("Entrada gratuita.");
+        } else if (edad >= 4 && edad <= 18) {
+            System.out.println("El precio de la entrada es $110.");
+        } else {
+            System.out.println("El precio de la entrada es $190.");
+        }
+    }
+
 }
