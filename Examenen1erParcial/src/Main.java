9451299e38e36ad3a97f9a909eb63dba8997fcf2
mrMain371 <luisitogvalle@gmail.com>
2025-02-04 14:34:07 -0600

    se realizo el examen

---
+++ Examenen1erParcial/src/Main.java
@@ -0,0 +1,12 @@
+import static ejercicio1.ejercicio1.ejercicio;
+import static ejercicio2.ejercicio2.ejercicio2;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+ejercicio();
+        System.out.println("ahora el ejercicio 2");
+        ejercicio2();
+    }
+}
